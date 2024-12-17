f1ad8f6c0a15e31973122a8f9bbfd71778843835
SYED FAIZAN ALI <faizanbhai0409@gmail.com>
2024-12-17 11:03:01 +0500

    1st Data

---
+++ script.js
@@ -0,0 +1,49 @@
+let allQuestions =  [
+    {
+        question : " CPU stands for?" ,
+        options : [
+            { answer : " Central Processing Unit   " , iscorrect: true},
+            { answer : "  Central Processor Unit   " , iscorrect: false},
+            { answer : "   Central Percent Unit " , iscorrect: false},
+            { answer : "  Central Percentage Unit  " , iscorrect: false},
+        ],
+    },
+
+    {
+        question : " PC stands for?" ,
+        options : [
+            { answer : " Personal  Computer ]   " , iscorrect: true},
+            { answer : "  Panther Computer   " , iscorrect: false},
+            { answer : " Pixels Computer  " , iscorrect: false},
+        ],
+    },
+
+    {
+        question : " HTML  stands for?" ,
+        options : [
+            { answer : " high transfer  mark language   " , iscorrect: false},
+            { answer : "  higher text mark langusge    " , iscorrect: false},
+            { answer : "   hyper text mark language  " , iscorrect: false},
+            { answer : "hyper text markup langu8age " , iscorrect: true},
+        ],
+    },
+
+    {
+        question : " CSS stands for?" ,
+        options : [
+            { answer : " Cascading Style Sheet   " , iscorrect: true},
+            { answer : "   Cascode Style Sheet  " , iscorrect: false},
+            { answer : "  Code Style Sheet   " , iscorrect: false},
+        ],
+    },
+
+    {
+        question : " JS  stands for?" ,
+        options : [
+            { answer : " Javascript   " , iscorrect: true},
+            { answer : "  Javapaste  " , iscorrect: false},
+        ],
+    }
+]
+
+console.log(allQuestions)
